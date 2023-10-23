b748284f6cbea6a9ec0d411029b787dca2c276d5
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-10-24 00:38:01 +0200

    Start implementing hash_table
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,1 +1,22 @@
+/**
+ * Hash table implementation
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
 #include "hash_table.h"
+
+/**
+ * Create a new hash table item
+ *
+ * @param k Key
+ * @param v Value
+ * @return ht_item
+ */
+static ht_item *ht_new_item(const char *k, const char *v) {
+  ht_item *i = malloc(sizeof(ht_item));
+  i->key = strdup(k);
+  i->value = strdup(v);
+  return i;
+}
--- src/hash_table.h
+++ src/hash_table.h
@@ -1,4 +1,30 @@
+/**
+ * Hash table data structure
+ *
+ * @file hash_table.h
+ * @brief Hash table data structure
+ */
+
+/**
+ * @brief Hash table item
+ *
+ * @param key Key of the item
+ * @param value Value of the item
+ */
 typedef struct {
   char *key;
   char *value;
 } ht_item;
+
+/**
+ * @brief Hash table
+ *
+ * @param size Size of the hash table
+ * @param count Number of items in the hash table
+ * @param items Items in the hash table
+ */
+typedef struct {
+  int size;
+  int count;
+  ht_item **items;
+} ht_hash_table;
